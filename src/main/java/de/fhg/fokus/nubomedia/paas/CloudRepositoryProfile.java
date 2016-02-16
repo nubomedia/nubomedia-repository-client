5c0c2d6a2d68f6c2d179016b8918e2d431dac032
Alice Cheambe <alice.cheambe@fokus.fraunhofer.de>
2016-02-16 12:46:47 +0100

    code cleaning

--- src/main/java/de/fhg/fokus/nubomedia/paas/CloudRepositoryProfile.java
+++ src/main/java/de/fhg/fokus/nubomedia/paas/CloudRepositoryProfile.java
@@ -8,6 +8,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  *
+ * @author Alice Cheambe <alice.cheambe[at]fokus.fraunhofer.de>
  *******************************************************************************/
 package de.fhg.fokus.nubomedia.paas;
 
