a3f6b7797d6cfa259c4ee17b9af142b1a56bb02a
Alice Cheambe <alice.cheambe@fokus.fraunhofer.de>
2016-02-25 19:48:32 +0100

    getting ready to release

--- src/main/java/de/fhg/fokus/nubomedia/PaasRepositoryProvider.java
+++ src/main/java/de/fhg/fokus/nubomedia/PaasRepositoryProvider.java
@@ -7,7 +7,7 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
- *
+ * @author Alice Cheambe [alice.cheambe[at]fokus.fraunhofer.de]
  *******************************************************************************/
 package de.fhg.fokus.nubomedia;
 
@@ -19,7 +19,7 @@ import de.fhg.fokus.nubomedia.paas.CloudRepositoryProfile;
 
 /**
  * Used to obtain the URL for the NUBOMEDIA cloud repository 
- * @author Alice Cheambe <alice.cheambe[at]fokus.fraunhofer.de>
+ * 
  *
  */
 public class PaasRepositoryProvider implements RepositoryUrlProvider{
--- src/main/java/de/fhg/fokus/nubomedia/paas/CloudRepositoryProfile.java
+++ src/main/java/de/fhg/fokus/nubomedia/paas/CloudRepositoryProfile.java
@@ -8,7 +8,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  *
- * @author Alice Cheambe <alice.cheambe[at]fokus.fraunhofer.de>
+ * @author Alice Cheambe [alice.cheambe[at]fokus.fraunhofer.de]
  *******************************************************************************/
 package de.fhg.fokus.nubomedia.paas;
 
@@ -17,7 +17,7 @@ import org.slf4j.LoggerFactory;
 
 /**
  *
- *  @author Alice Cheambe <alice.cheambe[at]fokus.fraunhofer.de>
+ *  Cloud repository profile contains data to access the Kurento Repository Server
  * 
  */
 public class CloudRepositoryProfile {
